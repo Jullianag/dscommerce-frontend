0d81d9a96db27e13fa2a2b5c6fcd765492fc61fa
Jullianag <juh_g@hotmail.com>
2024-10-18 14:46:30 -0300

    Adding React Router Dom

---
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -0,0 +1,45 @@
+import './styles.css';
+import ProductDetailsCard from "../../../components/ProductDetailsCard";
+import ButtonPrimary from "../../../components/ButtonPrimary";
+import ButtonInverse from "../../../components/ButtonInverse";
+import {ProductDTO} from "../../../models/product.ts";
+
+const product: ProductDTO = {
+    id: 2,
+    name: "Smart TV",
+    description: "Esta TV é muito bonita",
+    imgUrl: "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/refs/heads/master/backend/img/2-big.jpg",
+    price: 2500.99,
+    categories: [
+        {
+            id: 2,
+            name: "Eletrônicos"
+        },
+        {
+            id: 3,
+            name: "Computadores"
+        },
+        {
+            id: 4,
+            name: "Importados"
+        }
+    ]
+}
+
+export default function ProductDetails() {
+
+    return (
+
+        <main>
+            <section id="product-details-section" className="dsc-container">
+                <ProductDetailsCard product={product}/>
+                <div className="dsc-btn-page-container">
+                    <ButtonPrimary text="Comprar"/>
+                    <ButtonInverse text="Inicio"/>
+                </div>
+            </section>
+
+        </main>
+
+    );
+}
