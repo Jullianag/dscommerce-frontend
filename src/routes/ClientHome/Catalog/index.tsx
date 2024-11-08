d5847ad04c96872672f72605e82a9bed6ed685a8
Jullianag <juh_g@hotmail.com>
2024-11-08 16:17:27 -0300

    First back end requests

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -2,9 +2,22 @@ import './styles.css';
 import SearchBar from "../../../components/SearchBar";
 import CatalogCard from "../../../components/CatalogCard";
 import ButtonNextPage from "../../../components/ButtonNextPage";
+import {useEffect, useState} from "react";
+import {ProductDTO} from "../../../models/product.ts";
 import * as productService from '../../../services/product-service.ts';
 
 export default function Catalog() {
+
+    // ProductDTO[] pq é uma lista de produtos
+    const [products, setProducts] = useState<ProductDTO[]>([]);
+
+    useEffect(() => {
+        productService.findAll()
+            .then(response => {
+                // .content pq a resposta esta dentro da lista content (postman)
+                setProducts(response.data.content);
+            });
+    }, []);
 
     return (
         <main>
@@ -13,7 +26,7 @@ export default function Catalog() {
                 <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
                     {
                         // Não esquecer de uma "key" para coleções
-                        productService.findAll().map(
+                        products.map(
                             product => <CatalogCard key={product.id} product={product}/>
                         )
                     }
--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -2,14 +2,24 @@ import './styles.css';
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ButtonInverse from "../../../components/ButtonInverse";
-import * as productService from '../../../services/product-service.ts';
 import {Link, useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {ProductDTO} from "../../../models/product.ts";
+import * as productService from '../../../services/product-service.ts';
 
 export default function ProductDetails() {
 
     const params = useParams();
 
-    const product = productService.findById(Number(params.productId));
+    // declara o estado
+    const [product, setProduct] = useState<ProductDTO>();
+
+    useEffect(() => {
+        productService.findById(Number(params.productId))
+            .then(response => {
+                setProduct(response.data);
+            });
+    }, []);
 
     return (
 
